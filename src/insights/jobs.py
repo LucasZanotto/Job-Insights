5ac005207f7fa6619c3ad033778828b436a712f5
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-01-24 15:40:53 -0300

    feat: criando a função que filtra os jobs

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -22,7 +22,6 @@ def get_unique_job_types(path: str) -> List[str]:
         jobs_list = csv.DictReader(jobs_file)
         jobs_add = []
         for jobs in jobs_list:
-            # list = frozenset(jobs["job_type"])
             jobs_add.append(jobs["job_type"])
             job_set_unique = set(jobs_add)
 
@@ -31,23 +30,34 @@ def get_unique_job_types(path: str) -> List[str]:
     return jobs
 
 
-get_unique_job_types("data/jobs.csv")
-
-
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    get_unique_job_types("data/jobs.csv")
-    """Filters a list of jobs by job_type
+    jobs_list = jobs
+    jobs_add = []
+    for job in jobs_list:
+        if job["job_type"] == job_type:
+            jobs_add.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    return jobs_add
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+
+if __name__ == "__main__":
+    print(get_unique_job_types("data/jobs.csv"))
+    print(
+        filter_by_job_type(
+            [
+                {"id": 1, "job_type": "PART_TIME"},
+                {"id": 2, "job_type": "PART_TIME"},
+                {"id": 3, "job_type": "OTHER"},
+                {"id": 4, "job_type": "OTHER"},
+                {"id": 5, "job_type": "FULL_TIME"},
+                {"id": 6, "job_type": "FULL_TIME"},
+                {"id": 7, "job_type": "CONTRACTOR"},
+                {"id": 8, "job_type": "CONTRACTOR"},
+                {"id": 9, "job_type": "TEMPORARY"},
+                {"id": 10, "job_type": "TEMPORARY"},
+                {"id": 11, "job_type": "INTERN"},
+                {"id": 12, "job_type": "INTERN"},
+            ],
+            "TEMPORARY",
+        )
+    )
