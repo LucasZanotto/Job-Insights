3d105d48fdc01f122485631e41000d7ad18c4720
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-01-24 23:11:16 -0300

    fix: enviando o resto da função que traduz

--- src/pre_built/brazilian_jobs.py
+++ src/pre_built/brazilian_jobs.py
@@ -24,3 +24,7 @@ def read_brazilian_file(path: str) -> List[Dict]:
         job["type"] = job.pop("tipo")
 
     return dict_jobs
+
+
+if __name__ == "__main__":
+    print(read_brazilian_file("tests/mocks/brazilians_jobs.csv"))
