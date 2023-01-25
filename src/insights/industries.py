aa99d5493583a3571922f6f58759fedd711db8cc
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-01-24 23:17:00 -0300

    fix: arrumando a função que retorna industrias

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -4,11 +4,12 @@ from src.insights.jobs import read
 
 def get_unique_industries(path: str) -> List[str]:
     jobs_list = read(path)
-    industry_add = []
+    industry_add = set()
     for jobs in jobs_list:
         if len(jobs["industry"]) > 0:
-            industry_add.append((jobs["industry"]))
-    return len(industry_add)
+            industry_add.add((jobs["industry"]))
+    print(industry_add)
+    return industry_add
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
