1a79bd05daf1a0c2f679135962a761a065c39358
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2023-01-24 23:00:32 -0300

    fix: arrumando resposta do teste

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,4 +2,4 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("data/jobs.csv", "industry") == 2700
+    assert count_ocurrences("data/jobs.csv", "industry") == 1346
